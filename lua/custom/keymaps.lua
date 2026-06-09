4de61c0683362fade1a0919ed88ab43177b2dc25
Hajnal Máté <hajnalmt@gmail.com>
2026-06-09 13:38:22 +0200

    refactor(config): split init.lua into custom modules
    Extract the editor options, basic keymaps, and autocommands out of the
    monolithic init.lua into dedicated files under lua/custom/ and load them
    via require. This keeps init.lua focused on the plugin list and makes the
    config easier to navigate and to rebase on upstream kickstart.
    
    Also add a command-line mapping so <CR> accepts the highlighted wildmenu
    match, and rename the copy-path keymap to <leader>cfp.
    
    Signed-off-by: Hajnal Máté <hajnalmt@gmail.com>

--- lua/custom/keymaps.lua
+++ lua/custom/keymaps.lua
@@ -1,4 +1,43 @@
--- Netrw control
+-- [[ Basic Keymaps ]]
+--  See `:help vim.keymap.set()`
+
+-- Clear highlights on search when pressing <Esc> in normal mode
+--  See `:help hlsearch`
+vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+
+-- Diagnostic keymaps
+vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
+
+-- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
+-- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
+-- is not what someone will guess without a bit more experience.
+--
+-- NOTE: This won't work in all terminal emulators/tmux/etc. Try your own mapping
+-- or just use <C-\><C-n> to exit terminal mode
+vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
+
+-- TIP: Disable arrow keys in normal mode
+-- vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
+-- vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
+-- vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
+-- vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
+
+-- Keybinds to make split navigation easier.
+--  Use CTRL+<hjkl> to switch between windows
+--
+--  See `:help wincmd` for a list of all window commands
+vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
+vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
+vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
+vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+
+-- NOTE: Some terminals have colliding keymaps or are not able to send distinct keycodes
+-- vim.keymap.set("n", "<C-S-h>", "<C-w>H", { desc = "Move window to the left" })
+-- vim.keymap.set("n", "<C-S-l>", "<C-w>L", { desc = "Move window to the right" })
+-- vim.keymap.set("n", "<C-S-j>", "<C-w>J", { desc = "Move window to the lower" })
+-- vim.keymap.set("n", "<C-S-k>", "<C-w>K", { desc = "Move window to the upper" })
+
+-- [[ Netrw control ]]
 vim.keymap.set('n', '<leader>pv', '<cmd>Explore %:p:h<CR>', { desc = '[P]roject [V]iew current file dir' })
 
 vim.keymap.set('n', '<leader>pc', function()
@@ -19,4 +58,11 @@ vim.keymap.set('n', '<leader>pd', function()
 end, { desc = '[P]roject create [D]irectory' })
 
 -- Personal keymaps
-vim.keymap.set('n', '<leader>cp', ':let @+=expand("%:p")<CR>', { desc = 'Copy file path' })
+vim.keymap.set('n', '<leader>cfp', ':let @+=expand("%:p")<CR>', { desc = '[C]opy [F]ile [P]ath' })
+
+-- Use <CR> to accept the highlighted command-line completion.
+-- When the wildmenu is open, <CR> accepts the selection (built-in <C-y>);
+-- otherwise it behaves normally and runs the typed command.
+vim.keymap.set('c', '<CR>', function()
+  return vim.fn.wildmenumode() == 1 and '<C-y>' or '<CR>'
+end, { expr = true })
