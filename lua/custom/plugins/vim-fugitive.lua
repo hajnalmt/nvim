44878361e47c491813935ae7fa2c110ed861f3ec
Hajnal Máté <hajnalmt@gmail.com>
2026-09-16 10:48:16 +0200

    refactor(vim-fugitive): reorganize git bindings and add auto-commit shortcuts
    Reorganize commit commands under gc prefix for better grouping:
    - gcs: commit signed
    - gca: commit amend signed
    - gcA: commit amend no-edit signed
    
    Add new git operations:
    - gaa: add all
    - gg: add all + amend no-edit + push (normal workflow)
    - gG: add all + amend no-edit + force push (rewrite workflow)
    
    Signed-off-by: Hajnal Máté <hajnalmt@gmail.com>

--- lua/custom/plugins/vim-fugitive.lua
+++ lua/custom/plugins/vim-fugitive.lua
@@ -9,14 +9,16 @@ return {
     -- Clear exepath cache so it uses the new PATH
     vim.fn.exepath('gh')
   end,
-  keys = {
-    { '<leader>gs', '<cmd>Git<CR>', desc = '[G]it [S]tatus' },
-    { '<leader>gds', '<cmd>Gdiffsplit<CR>', desc = '[G]it [D]iff [S]plit' },
-    { '<leader>gc', '<cmd>Git commit -s<CR>', desc = '[G]it [C]ommit signed' },
-    { '<leader>ga', '<cmd>Git commit --amend -s<CR>', desc = '[G]it [A]mend signed' },
-    { '<leader>gA', '<cmd>Git commit --amend --no-edit -s<CR>', desc = '[G]it [A]mend no-edit signed' },
-    { '<leader>gb', '<cmd>Git blame<CR>', desc = '[G]it [B]lame' },
-    { '<leader>gp', '<cmd>Git push -u origin HEAD<CR>', desc = '[G]it [P]ush current branch' },
-    { '<leader>gP', '<cmd>Git push --force -u origin HEAD<CR>', desc = '[G]it [P]ush current branch' },
-  },
+   keys = {
+     { '<leader>gs', '<cmd>Git<CR>', desc = '[G]it [S]tatus' },
+     { '<leader>gds', '<cmd>Gdiffsplit<CR>', desc = '[G]it [D]iff [S]plit' },
+     { '<leader>gcs', '<cmd>Git commit -s<CR>', desc = '[G]it [C]ommit [S]igned' },
+     { '<leader>gca', '<cmd>Git commit --amend -s<CR>', desc = '[G]it [C]ommit [A]mend signed' },
+     { '<leader>gcA', '<cmd>Git commit --amend --no-edit -s<CR>', desc = '[G]it [C]ommit [A]mend no-edit signed' },
+     { '<leader>gaa', '<cmd>Git add --all<CR>', desc = '[G]it [A]dd [A]ll' },
+     { '<leader>gb', '<cmd>Git blame<CR>', desc = '[G]it [B]lame' },
+     { '<leader>gp', '<cmd>Git push -u origin HEAD<CR>', desc = '[G]it [P]ush current branch' },
+     { '<leader>gG', '<cmd>Git add --all<CR> | <cmd>Git commit --amend --no-edit -s<CR> | <cmd>Git push --force -u origin HEAD<CR>', desc = '[G]it auto-[G]o (add, amend, force push)' },
+     { '<leader>gg', '<cmd>Git add --all<CR> | <cmd>Git commit --amend --no-edit -s<CR> | <cmd>Git push -u origin HEAD<CR>', desc = '[G]it auto-[G]o (add, amend, push)' },
+   },
 }
